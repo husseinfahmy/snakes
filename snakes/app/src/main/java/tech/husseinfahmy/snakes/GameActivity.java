a706bc01edb578f1d7ff24054eed875fae47deba
Hussein Fahmy <fahmy.hussein@hotmail.com>
2016-05-14 16:26:30 -0400

    page linking

---
+++ snakes/app/src/main/java/tech/husseinfahmy/snakes/GameActivity.java
@@ -0,0 +1,23 @@
+package tech.husseinfahmy.snakes;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game2);
+
+        RelativeLayout layout = new RelativeLayout(this);
+        ImageView headNode = new ImageView(this);
+        
+
+
+    }
+}
--- snakes/app/src/main/java/tech/husseinfahmy/snakes/MainActivity.java
+++ snakes/app/src/main/java/tech/husseinfahmy/snakes/MainActivity.java
@@ -1,7 +1,10 @@
 package tech.husseinfahmy.snakes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        Intent intent = new Intent(v.getContext(), GameActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+        );
     }
 }
